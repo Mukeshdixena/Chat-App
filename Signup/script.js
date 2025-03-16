6f6b9c77f043061cec0293299b219c7391a689e5
Mukesh <mukeshdixena123@gmail.com>
2025-03-16 11:57:03 +0530

    integrated with signup api

--- Signup/script.js
+++ Signup/script.js
@@ -1,4 +1,4 @@
-document.getElementById('signupForm').addEventListener('submit', function (event) {
+document.getElementById('signupForm').addEventListener('submit', async function (event) {
     event.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
@@ -6,5 +6,9 @@ document.getElementById('signupForm').addEventListener('submit', function (event
     const password = document.getElementById('password').value;
 
     console.log({ name, email, phone, password });
-    alert("Signup Successful!");
+
+    const response = await axios.post(`${CONFIG.API_BASE_URL}/api/postUser`, {
+        username: name, email: email, phonenumber: phone, password: password
+    });
+    console.log(response);
 });
