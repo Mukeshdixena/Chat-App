a681c94af55271213080bc326e02533b2659dd1f
Mukesh <mukeshdixena123@gmail.com>
2025-03-16 11:39:47 +0530

    created signin page

---
+++ Signup/script.js
@@ -0,0 +1,10 @@
+document.getElementById('signupForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const password = document.getElementById('password').value;
+
+    console.log({ name, email, phone, password });
+    alert("Signup Successful!");
+});
