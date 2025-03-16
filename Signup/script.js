ef2fa314ed2a18b1bae685a55b5a0f67525f27cc
Mukesh <mukeshdixena123@gmail.com>
2025-03-16 14:57:47 +0530

    real time updated in every single secont

--- Signup/script.js
+++ Signup/script.js
@@ -1,14 +1,24 @@
 document.getElementById('signupForm').addEventListener('submit', async function (event) {
     event.preventDefault();
+
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
     const password = document.getElementById('password').value;
 
-    console.log({ name, email, phone, password });
+    const userData = { username: name, email, phonenumber: phone, password };
 
-    const response = await axios.post(`${CONFIG.API_BASE_URL}/api/postUser`, {
-        username: name, email: email, phonenumber: phone, password: password
-    });
-    console.log(response);
+    console.log(userData);
+
+    try {
+        const response = await axios.post(`${CONFIG.API_BASE_URL}/api/postUser`, userData);
+
+        if (response.status === 201) {
+            // Redirect to the sign-in page on success
+            window.location.href = "../index.html"; // Adjust this URL based on your sign-in page path
+        }
+    } catch (error) {
+        console.error('Error submitting the form:', error);
+        // Handle error, e.g., show a message to the user
+    }
 });
